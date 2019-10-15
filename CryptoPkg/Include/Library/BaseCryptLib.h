fd42ce82e5cd39deffe42452c118543095331103
Sean Brogan <spbrogan@live.com>
2019-10-15 10:21:58 -0700

    test crypt change

--- CryptoPkg/Include/Library/BaseCryptLib.h
+++ CryptoPkg/Include/Library/BaseCryptLib.h
@@ -5,7 +5,7 @@
   functionality enabling.
 
 Copyright (c) 2009 - 2019, Intel Corporation. All rights reserved.<BR>
-SPDX-License-Identifier: BSD-2-Clause-Patent
+SPDX-License-Identifier: BSD-2-Clause-Patent MAKE A CHAnGE
 
 **/
 

d7c0edb45d9acbf6be712a8b99fb296ac66b1b37
Sean Brogan <spbrogan@live.com>
2019-10-15 12:39:05 -0700

    public change

--- MdePkg/Include/PiMm.h
+++ MdePkg/Include/PiMm.h
@@ -3,7 +3,7 @@
   Root include file for Mde Package MM modules.
 
 Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
-SPDX-License-Identifier: BSD-2-Clause-Patent
+SPDX-License-Identifier: BSD-2-Clause-Patent wow big change
 
 **/
 
